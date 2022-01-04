a322a33f79278442a3e48b26d8dd1782415665a9
Alex Beliaev <glorrianss@outlook.com>
2022-01-04 15:25:48 +0200

    Initial Commit

---
+++ ContactBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactBook
+//
+//  Created by Alex on 04.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
